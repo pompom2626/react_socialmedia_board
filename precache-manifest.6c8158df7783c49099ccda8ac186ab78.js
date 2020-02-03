c6d13ce089495440f14ff4bbb066c93324d01e13
pompom2626 <pompom2626@gmail.com>
2020-02-02 23:41:37 -0600

    Updates

---
+++ precache-manifest.6c8158df7783c49099ccda8ac186ab78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb786171b9ca4b4620987cbc6655ec08",
+    "url": "/react_socialmedia_board/index.html"
+  },
+  {
+    "revision": "8f294d223f1c26efae07",
+    "url": "/react_socialmedia_board/static/css/main.e954d397.chunk.css"
+  },
+  {
+    "revision": "318a4f282ff61d95e82b",
+    "url": "/react_socialmedia_board/static/js/2.bbb0b1ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_socialmedia_board/static/js/2.bbb0b1ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f294d223f1c26efae07",
+    "url": "/react_socialmedia_board/static/js/main.0e2815f4.chunk.js"
+  },
+  {
+    "revision": "bbfc42493d3e95cf095d",
+    "url": "/react_socialmedia_board/static/js/runtime-main.f3eed458.js"
+  }
+]);
